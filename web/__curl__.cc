c88d4b02b18a85f184854a80d25a458e929d7fd3
Sahil Yadav <yadavsahil5198@gmail.com>
2018-05-08 02:47:54 +0530

    Return the response of the query to the function

--- web/__curl__.cc
+++ web/__curl__.cc
@@ -4,15 +4,15 @@
 
 DEFUN_DLD (__curl__, args, , "Some __curl__ demo.")
 {
-  if (args.length () != 1)
+  if (args.length () < 1)
     print_usage ();
 
-  auto a = libcurl_wrapper::create();
+  std::string tmp_file = args(1).string_value();
+  auto a = libcurl_wrapper::create(tmp_file);
 
   std::string wikiapi = "http://wiki.octave.org/wiki/api.php";
   std::string url =  wikiapi +  "?action=query&meta=tokens&type=login&format=json";
   a.setURL (url);
-  a.setWRITEFUNCTION ();
   a.perform ();
 
   return octave_value (a.getTOKEN ());
